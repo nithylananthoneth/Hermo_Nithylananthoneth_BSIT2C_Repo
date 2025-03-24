07706704bc5ade9a617bc5f9fdae7fdb866ea01a
nithylanhermo <nithylanhermo@gmail.com>
2025-03-24 00:45:48 -0700

    Functional Activity 1

---
+++ Activities/script.js
@@ -0,0 +1,13 @@
+function whatshalliwear (temperature) {
+    if (temp < 60) {
+        console.log("wear a jacket");
+    } else if (temp < 70){
+        console.log("wear a sweater")
+    } else {
+        console.log("wear a t-shirt")
+    }
+}
+
+whatshalliwear(50)
+whatshalliwear(80)
+whatshalliwear(60)
